30b921b440ab19126123509437b8e3d936e668e0
Jaime Navarrete <jaimenava123@outlook.com>
2020-12-28 00:32:58 -0600

    Agregado - Componente de proceso de inscripcion - Pagina de inscripcion

--- src/Pages/SubjectRegistration.js
+++ src/Pages/SubjectRegistration.js
@@ -5,8 +5,30 @@ function SubjectRegistration() {
         <main className="bg-gray-200 min-h-screen p-5">
             <h2 className="subtitle-1 text-gray-700">Inscripción de materias</h2>
             <div className="max-w-7xl mx-auto">
+                <form className="card">
+                    <h3 className="subtitle-2 mb-10">Proceso de inscripción</h3>
+                    <label className="mb-5 block">
+                        <span className="text-blue-500 font-bold block mb-3">Materia:</span>
+                        <select className="font-bold border-2 rounded w-full max-w-xl">
+                            <option value="LIS104">Lenguajes Interp. en el Servidor</option>
+                        </select>
+                    </label>
+                    <label className="mb-5 block">
+                        <span className="text-blue-500 font-bold block mb-3">Grupo teórico:</span>
+                        <select className="font-bold border-2 rounded w-32">
+                            <option value="LIS104">01T</option>
+                        </select>
+                    </label>
+                    <label className="mb-5 block">
+                        <span className="text-blue-500 font-bold block mb-3">Cupos disponibles:</span>
+                        <input type="text" value="1" className="bg-gray-100 text-gray-500 font-bold border-2 rounded w-32" readOnly />
+                    </label>
+                    <div className="flex justify-end">
+                        <button className="btn"><i className="las la-plus"></i> Agregar materia</button>
+                    </div>
+                </form>
                 <section className="card">
-                    <h3 className="subtitle-2 mb-10">Grupos de materias</h3>
+                    <h3 className="subtitle-2 mb-10">Materias a inscribir</h3>
                 </section>
             </div>
         </main>
